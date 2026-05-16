28203a6604bb9d7ae684910703c071b73ccb50ea
Ashutosh Chaturvedi <reach.a.chaturvedi@gmail.com>
2026-05-17 01:56:23 +0530

    api integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Search from './components/Search.jsx'
+import Spinner from './components/Spinner.jsx'
 
 const API_BASE_URL = '/api';
 
@@ -24,18 +25,40 @@ const App = () => {
 
   const[errorMessage, setErrorMessage] = useState('');
 
+  const[movieList, setMovieList] = useState([]);
+
+  const[isLoading, setIsLoading] = useState(false);
+
   const fetchMovies = async () => {
+
+    setIsLoading(true);
+    setErrorMessage('');
     
     try{
       const endpoint = `${API_BASE_URL}/movies/popular?extended=full`;
 
       const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok){
+        throw new Error('Failed to fetch movies');
+      }
       
       const data = await response.json();
+
+      if(!data || data.length === 0){
+        setErrorMessage('Failed to fetch  movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data || []);
+      
     }
     catch(error){
       console.log(`Error fetching movies : ${error}`);
       setErrorMessage('Error fetching movies, Please try again later.');
+    } finally{
+      setIsLoading(false);
     }
   }
 
@@ -55,10 +78,20 @@ const App = () => {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
 
-        <section className='all movies'>
-          <h2>All Movies</h2>
+        <section className='all-movies'>
+          <h2 className="mt-10">All Movies</h2>
 
-          {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+            {isLoading ? (
+              <Spinner/>
+            ) : errorMessage ? (
+              <p className="text-red-500">{errorMessage}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  <p key={movie.ids.tmdb} className="text-white">{movie.title}</p>
+                ))}
+              </ul>  
+            )}
         </section>
       </div>
     </main>
